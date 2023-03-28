01ab73abceda345ee5f9b35965ba880380fc0d84
BornToBeLoved <sungwon1685@gmail.com>
2023-03-29 01:31:17 +0900

    #9 implemented chatbot class which contains scenarios

--- Middleware/chatbot.py
+++ Middleware/chatbot.py
@@ -1,12 +1,15 @@
 import sys
 import os
+
+
 sys.path.append(os.path.dirname('/Users/iseong-won/model-assistant-chatgpt/keys'))
 sys.path.append(os.path.dirname('/Users/iseong-won/model-assistant-chatgpt/scenarios'))
 sys.path.append(os.path.dirname('/Users/iseong-won/model-assistant-chatgpt/models'))
-
 import openai
 import keys.openai_apy_key as key
 import scenarios.gpt_system_content as content
+
+import json
 
 # 발급받은 API 키 설정
 OPENAI_API_KEY = key.key
@@ -14,37 +17,65 @@ OPENAI_API_KEY = key.key
 # openai API 키 인증
 openai.api_key = OPENAI_API_KEY
 
-# 모델 - GPT 3.5 Turbo 선택
-model = "gpt-3.5-turbo"
+class Chatbot:
+    def __init__(self, username):
+        # 모델 - GPT 3.5 Turbo 선택
+        self.model = "gpt-3.5-turbo"
+        self.username = username
+        self.user_path = '/Users/iseong-won/model-assistant-chatgpt/userfiles' + f"/{self.username}"
+        self.messages = self.load_message()
 
-# ChatGPT API 호출
-# 메시지(문맥) 설정하기
-def make_first_content(text):
-    messages = [
-            {"role": "system", "content": text}
-    ]
-    return messages
-
-def talk_with_bot( query, messages = None, debug = False):
-    if debug:
-        print(query)
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    answer = response['choices'][0]['message']['content']
+    # ChatGPT API 호출
+    # 메시지(문맥) 설정하기
+    def make_first_content(self):
+        messages = [
+                {"role": "system", "content": content.before_predict}
+        ]
+        return messages
     
-    if debug:
-        print(answer)
-    return answer
+    def save_message(self, data = None):
+        if data == None:
+            data = self.make_first_content()
 
-messages = make_first_content(content.content)
+        with open(self.user_path+"/messages.json", "w") as f:
+            json.dump(data, f)
 
-while True: 
-    query = input()
-    if query =='quit':
-        break
-    messages.append({"role": "user", "content": query})
+    def load_message(self):
+        if not os.path.exists(self.user_path):
+            os.mkdir(self.user_path)
+            self.save_message()
+    
+        with open(self.user_path+"/messages.json", "r") as f:
+            data = json.load(f)
+        return data
 
-    answer = talk_with_bot(query, messages, debug=True)
-    messages.append({"role": "assistant", "content": answer})
+    def talk_with_bot(self, query, debug = False):
+        if debug:
+            print(query)
+        
+        self.messages.append({"role": "user", "content": query})
+
+        response = openai.ChatCompletion.create(
+            model= self.model,
+            messages=self.messages
+        )
+        answer = response['choices'][0]['message']['content']
+
+        self.messages.append({"role": "assistant", "content": answer})
+
+        
+        self.save_message(data = self.messages)
+
+        if debug:
+            print(answer)
+
+        return answer
+
+
+'''
+USER차원의 변수
+'''
+while True:
+    bot1 = Chatbot("1")
+    query = input("입력:")
+    print(bot1.talk_with_bot(query))
